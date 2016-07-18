b89aa4cb1bdbc14ac5e5e9e57997dc8304663f45
Matt Lawes <mlawes390@gmail.com>
2016-07-18 16:28:05 +0800

    Updated with header information, TimeoutError interupt and system exit

--- Data_Acquisition.py
+++ Data_Acquisition.py
@@ -1,22 +1,54 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+"""
+Reads accererometer data sent through via the sensor node and
+saves as a .csv file
+
+Usage: Data_Acquisition.py [options]
+
+Options:
+	-o=SAVE_FILE           The file to write to [Default: ./data.csv]
+	-b=BAUD --baud=BAUD    The Baud Rate [Default: 9600]
+	-p=PORT --port=PORT    The serial port to listen to [Default: /dev/ttyUSB0]
+	-t=TIMEOUT --timeout=TIMEOUT
+	                       Set the timeout for the read operation [Default: 5]
+	-h --help              Show this help information
+	-V --version           Print the version information
+"""
+__version__ = '0.1.0'
 
 import serial
 import struct
 import csv
 import time
+import sys
+import docopt
 
-port = '/dev/ttyUSB0'
-baud = 9600
+def main(args):
+    save_file = args['-o']
+    
+    # Open serial connection with node
+    ser = serial.Serial(args['--port'],
+                        baud=int(args['--baud']),
+                        timeout=int(args['--timeout']))
+    # Allow time for serial to initlise and send start byte
+    time.sleep(10)
+    ser.write(b's')
 
-savefile = '/home/matthew/Desktop/Unprocessed data/test.csv'
-
-ser = serial.Serial(port, baud)
-time.sleep(10)
-ser.write('s')
-
-with open(savefile, 'wb') as f:
-	writer = csv.writer(f)
-	while 1:
-		raw = ser.read(16)
-		data = struct.unpack('Ifff',raw)
-		writer.writerow(data)
+    # open .csv file with write
+    with open(save_file, 'w') as f:
+	    writer = csv.writer(f)
+	    
+	    try:
+	        while True:
+		        raw = ser.read(16)
+		        data = struct.unpack('Ifff',raw)
+		        writer.writerow(data)
+        except KeyboardInterrupt, TimeoutError:
+            # end read when keyboard interrupt or no data
+            # send in 5 seconds
+            print('Program end')
+            sys.exit(0)
+            
+if __name__ == '__main__':
+    args = docopt.docopt(__doc__, version=__version__)
+    main(args)
