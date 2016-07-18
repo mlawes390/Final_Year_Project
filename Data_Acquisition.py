8e44f522b448746068ba1997b7796a4b35b1369b
Matt Lawes <mlawes390@gmail.com>
2016-07-18 17:48:54 +0800

    General bug fixing

--- Data_Acquisition.py
+++ Data_Acquisition.py
@@ -6,13 +6,13 @@ saves as a .csv file
 Usage: Data_Acquisition.py [options]
 
 Options:
-	-o=SAVE_FILE           The file to write to [Default: ./data.csv]
-	-b=BAUD --baud=BAUD    The Baud Rate [Default: 9600]
-	-p=PORT --port=PORT    The serial port to listen to [Default: /dev/ttyUSB0]
-	-t=TIMEOUT --timeout=TIMEOUT
-	                       Set the timeout for the read operation [Default: 5]
-	-h --help              Show this help information
-	-V --version           Print the version information
+    -o=SAVE_FILE           The file to write to [Default: ./data.csv]
+    -b=BAUD --baud=BAUD    The Baud Rate [Default: 9600]
+    -p=PORT --port=PORT    The serial port to listen to [Default: /dev/ttyUSB0]
+    -t=TIMEOUT --timeout=TIMEOUT
+                           Set the timeout for the read operation [Default: 5]
+    -h --help              Show this help information
+    -V --version           Print the version information
 """
 __version__ = '0.1.0'
 
@@ -24,11 +24,11 @@ import sys
 import docopt
 
 def main(args):
-    save_file = args['-o']
+    save_file = args.get('-o', './data.csv')
     
     # Open serial connection with node
     ser = serial.Serial(args['--port'],
-                        baud=int(args['--baud']),
+                        baudrate=int(args['--baud']),
                         timeout=int(args['--timeout']))
     # Allow time for serial to initlise and send start byte
     time.sleep(10)
@@ -36,13 +36,13 @@ def main(args):
 
     # open .csv file with write
     with open(save_file, 'w') as f:
-	    writer = csv.writer(f)
-	    
-	    try:
-	        while True:
-		        raw = ser.read(16)
-		        data = struct.unpack('Ifff',raw)
-		        writer.writerow(data)
+        writer = csv.writer(f)
+        
+        try:
+            while True:
+                raw = ser.read(16)
+                data = struct.unpack('Ifff',raw)
+                writer.writerow(data)
         except KeyboardInterrupt, TimeoutError:
             # end read when keyboard interrupt or no data
             # send in 5 seconds
@@ -51,4 +51,5 @@ def main(args):
             
 if __name__ == '__main__':
     args = docopt.docopt(__doc__, version=__version__)
+    print(args)
     main(args)
