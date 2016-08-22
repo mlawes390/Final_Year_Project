76a65f27ef06ef2bc2980e40d922ac122a01fb14
mlawes390 <mlawes390@gmail.com>
2016-08-22 10:46:52 +0800

    Calibration and Power Saving
    Added function to handle calibration of accelerometer with the calibration matrix from Calibration.ino
    Added sleep mode (power down, lowest power consumption) with pin 2 interrupt wake.

--- Data_Acquisition.ino
+++ Data_Acquisition.ino
@@ -7,35 +7,51 @@
 
 #include <Wire.h>
 #include <SPI.h>
+#include <avr/interrupt.h>
+#include <avr/power.h>
+#include <avr/sleep.h>
 #include <Adafruit_LIS3DH.h>
 #include <Adafruit_Sensor.h>
+#include <MatrixMath.h>
+
 
 #define LIS3DH_CS 10
 // Hardware SPI
 Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS);
 
-union Elapsed{
+int pin2 = 2;
+int count=0;
+union Elapsed {
   unsigned long t_f;
   byte t_b[4];
-}Elapsed ;
+} Elapsed ;
 
-union x_acel{
+union x_acel {
   float x_f;
   byte x_b[4];
-}x_acel;
+} x_acel;
 
-union y_acel{
+union y_acel {
   float y_f;
   byte y_b[4];
-}y_acel;
+} y_acel;
 
-union z_acel{
+union z_acel {
   float z_f;
   byte z_b[4];
-}z_acel;
+} z_acel;
+
+float cal_mat[4][3] = {{1, 0, 0},
+  {0, 1, 0},
+  {0, 0, 1},
+  {0, 0, 0}
+};
 
 void setup(void) {
-  Serial.begin(9600);
+  Serial.begin(115200);
+
+  pinMode(pin2, INPUT);
+  attachInterrupt(0,WakeUp,LOW);
 
   lis.begin();
   lis.setRange(LIS3DH_RANGE_4_G);                 // 2, 4, 8 or 16 G!
@@ -43,25 +59,66 @@ void setup(void) {
 }
 
 void loop() {
+  count++;
+  delay(1000);
+  
   if (Serial.available()) {
 
     byte r = Serial.read();
 
     if (r == (byte)'s') {
       unsigned long startTime = micros();
-      for (int i = 0; i < 100; i++) {
+      for (int i = 0; i < 1024; i++) {
         Elapsed.t_f = micros() - startTime;
         lis.read();
         x_acel.x_f = lis.x_g;
         y_acel.y_f = lis.y_g;
         z_acel.z_f = lis.z_g;
+        calibration(x_acel.x_f, y_acel.y_f, z_acel.z_f);
         Serial.write(Elapsed.t_b, 4);
         Serial.write(x_acel.x_b, 4);
         Serial.write(y_acel.y_b, 4);
         Serial.write(z_acel.z_b, 4);
       }
     }
+    count=0;
+    delay(1000);
   }
-  delay(10);
+  if (count>=10) {
+    Serial.println("Entering Sleep");
+    delay(100);
+    count = 0;
+    sleep();
+  }
+}
+
+void calibration(float x_acel, float y_acel, float z_acel) {
+  /* Calibrate Accelerometer to give true readings */
+  float raw[4] = {x_acel, y_acel, z_acel, 1};
+  float act_g[3];
+
+  Matrix.Multiply((float*)raw, (float*)cal_mat, 1, 4, 3, (float*)act_g);
+
+  x_acel = act_g[0];
+  y_acel = act_g[1];
+  z_acel = act_g[2];
+}
+
+void WakeUp(){
+  //Null
+}
+void sleep() {
+  /* Sleep mode to requde Power consumption
+     Wake on pin 2 interrupt
+  */
+
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  sleep_enable();
+  attachInterrupt(0,WakeUp,LOW);
+  sleep_mode();
+
+  sleep_disable();
+  detachInterrupt(0);
+  /* Note on wake from serial, first byte is not read */
 }
 
