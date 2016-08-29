5d26cf3ef278b1d4a2087bd8fdc0a45f5626d964
Matt <mlawes390@gmail.com>
2016-08-29 20:30:17 +0800

    removed calls to Autonomous.py renamed program calls to reflect actual modules

--- Process all.py
+++ Process all.py
@@ -3,14 +3,56 @@ import subprocess
 import shlex
 import re
 import datetime
-from Autotonomous import gen_filename, rfcomms, acquisition
+
+def gen_filename(rfcomm, now):
+    """
+    Generate file name for rfcomm with timestamp.
+    """
+    match = re.search(r'\d+', rfcomm)
+    if match is None:
+        raise ValueError('Invalid rfcomm, {}'.format(rfcomm))
+
+    port = match.group(0)
+    now_str = now.strftime('%Y-%m-%d_%H:%M:%S')
+
+    filename = 'Acel_{}_{}.csv'.format(port, now_str)
+    return filename
+
+
+def rfcomms():
+    """
+    Get a list of all rfcomm on the system.
+    """
+    ports = []
+    for device in os.listdir('/dev/'):
+        if device.startswith('rfcomm'):
+            ports.append('/dev/' + device)
+    return ports
 
 
 def main():
-    config = {
-            }
-    acquisition(config)
+    """
+    Poll accelerometer, store collected data in .csv file and process data to produce
+    time domain and frequency domain figures and parameters.
+    """
+    data_directory = './Accelerometer_data'
 
+    for rfcomm in rfcomms():
+        now = datetime.datetime.now()
+        filename = gen_filename(rfcomm, now)
+        full_name = os.path.join(data_directory, filename)
+
+        # Poll sensor and save to .csv
+        cmd = 'python3 Data_Acquisition.py -o {} -p {}'.format(full_name, rfcomm)
+        cmd = shlex.split(cmd)
+        proc = subprocess.Popen(cmd)
+        proc.wait()
+
+        # Process data
+        cmd = 'python Processing.py -o {}'.format(full_name)
+        cmd = shlex.split(cmd)
+        proc = subprocess.Popen(cmd)
+        proc.wait()
 
 if __name__ == "__main__":
     main()
