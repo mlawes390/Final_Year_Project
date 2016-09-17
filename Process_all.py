5ba7a478db902e8a77960e401184d22377888259
Matt Lawes <mlawes390@gmail.com>
2016-09-17 15:58:21 +0800

    Added sequence prints for acquisition

--- Process_all.py
+++ Process_all.py
@@ -37,22 +37,28 @@ def main():
     """
     data_directory = './Accelerometer_data'
 
+    count = 1
+
     for rfcomm in rfcomms():
         now = datetime.datetime.now()
         filename = gen_filename(rfcomm, now)
         full_name = os.path.join(data_directory, filename)
 
         # Poll sensor and save to .csv
+        print('Sampling {}'.format(count))
         cmd = 'python3 Data_Acquisition.py -o {} -p {}'.format(full_name, rfcomm)
         cmd = shlex.split(cmd)
         proc = subprocess.Popen(cmd)
         proc.wait()
 
         # Process data
+        print('Processing {}'.format(count))
         cmd = 'python3 Processing.py -o {}'.format(full_name)
         cmd = shlex.split(cmd)
         proc = subprocess.Popen(cmd)
         proc.wait()
+
+        count += 1
 
 if __name__ == "__main__":
     main()
